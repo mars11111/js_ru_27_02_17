8c724edaf509168d7e6d92020069693bc6a12f2a
Roman <kaboom.ua@gmail.com>
2017-03-22 23:06:18 +0200

    Update commentIdGenerator.js

--- src/middlewares/commentIdGenerator.js
+++ src/middlewares/commentIdGenerator.js
@@ -4,7 +4,9 @@ export default store => next => action => {
     const { type, payload } = action
 
     switch (type) {
+        //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
         case ADD_COMMENT:
+            //и лучше не мутировать payload, мало-ли что там станут передавать
             payload.comment.id = new Date().getTime()
     }
 
